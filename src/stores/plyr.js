9b4531c4f5d74eff49903a12eee6be038823bf06
SIyuyuko <3228981717@qq.com>
2024-11-12 15:39:22 +0800

    fix bugs

--- src/stores/plyr.js
+++ src/stores/plyr.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { computed, ref, watch } from 'vue';
-import { getBeatmapBg, getBeatmapFile, getBeatmapSong } from '@/api/data_api.js';
+import { getBeatmapBg, getBeatmapFile } from '@/api/data_api.js';
 import { debounce } from "lodash";
 
 export const usePlyrStore = defineStore('plyr', () => {
@@ -86,7 +86,7 @@ export const usePlyrStore = defineStore('plyr', () => {
 	}, 2000));
 	watch(songUrl, debounce((val) => {
 		if (val) {
-			songUrl.value = `/bot/sp/file/map/song/${info.value.id}`;
+			songUrl.value = `/sp/file/map/song/${info.value.id}`;
 			updateFileUrl(info.value, songUrl.value, 'file');
 		}
 	}, 3000));
